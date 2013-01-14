748fea41adef8dcef5a9ed1cfca8f5bce28c4b72
roman.oreshko <oreshkor@gmail.com>
2013-01-14 20:11:47 +0300

    beforemerge

--- luckybird/PinnacleFeedParser.cs
+++ luckybird/PinnacleFeedParser.cs
@@ -8,6 +8,17 @@ namespace luckybird
 {
     public class PinnacleFeedParser
     {
+        static Func<IEnumerable<string>, double> getFirstAndParseToDouble = e => double.Parse(e.First());
+
+        readonly Func<int, string> _sportKindResonlver;
+        readonly Func<int, string> _leagueNameResolver;
+
+        public PinnacleFeedParser(Func<int, string> sportKindResolver, Func<int, string> leagueNameResolver)
+        {
+            _sportKindResonlver = sportKindResolver;
+            _leagueNameResolver = leagueNameResolver;
+        }
+
         public IEnumerable<FootballEvent> Parse(string file)
         {
             var xmlPathDoc = new XPathDocument(file);
@@ -15,7 +26,7 @@ namespace luckybird
             return NavigateSprts(xmlPathDoc.CreateNavigator());
         }
 
-        static IEnumerable<FootballEvent> NavigateSprts(XPathNavigator nav)
+        IEnumerable<FootballEvent> NavigateSprts(XPathNavigator nav)
         {
             // move to the root and the first element - <books>
             nav.MoveToRoot();
@@ -25,12 +36,12 @@ namespace luckybird
             while (sports.MoveNext())
             {
                 var sport = sports.Current;
-                var sportid = GetIdFromNode(sport);
+                var sportName = _sportKindResonlver(GetIdFromNode(sport));
                 var leagues = sport.Select("leagues/league");
 
                 while (leagues.MoveNext())
                 {
-                    var leagueid = GetIdFromNode(leagues.Current);
+                    var leagueName = _leagueNameResolver(GetIdFromNode(leagues.Current));
                     var events = leagues.Current.Select("events/event");
                     while (events.MoveNext())
                     {
@@ -42,39 +53,76 @@ namespace luckybird
                         var awayTeam = GetChildNodeValue(@event, "awayTeam/name").First();
 
                         var periods = @event.Select("periods/period");
-                        while (periods.MoveNext())
-                        {
-                            var period = periods.Current;
+                        var pperiods = ParsePeriods(periods).ToArray();
 
-                            var name = GetChildNodeValue(period, "description").First();
-                            var spreads = period.Select("spreads/spread");
-
-                            while (spreads.MoveNext())
-                            {
-                                var spread = spreads.Current;
-                                var awaySpread = double.Parse(GetChildNodeValue(spread, "awaySpread").First());
-                                var awayPrice = double.Parse(GetChildNodeValue(spread, "awayPrice").First());
-                                var homeSpread = double.Parse(GetChildNodeValue(spread, "homeSpread").First());
-                                var homePrice = double.Parse(GetChildNodeValue(spread, "homePrice").First());
-                            }
-
-                            var totals = period.Select("totals/total");
-
-                            while (totals.MoveNext())
-                            {
-                                var total = totals.Current;
-
-                                var points = double.Parse(GetChildNodeValue(total, "points").First());
-                                var overPrice = double.Parse(GetChildNodeValue(total, "overPrice").First());
-                                var underPrice = double.Parse(GetChildNodeValue(total, "underPrice").First());
-                            }
-                        }
+                        yield return new FootballEvent { AwayTeam = awayTeam, HomeTeam = homeTeam, League = leagueName, Sport = sportName, Periods = pperiods, When = when };
                     }
                 }
             }
-            return new FootballEvent[0];
+        }
+
+        private static IEnumerable<Period> ParsePeriods(XPathNodeIterator periods)
+        {
+
+            while (periods.MoveNext())
+            {
+                var period = periods.Current;
+
+                var name = GetChildNodeValue(period, "description").First();
+                var spreads = period.Select("spreads/spread");
+
+                var pperiods = ParseSpreads(spreads).ToArray();
+
+                var totals = period.Select("totals/total");
+
+                var ptotals = ParseTotals(totals).ToArray();
+
+                var moneyLine = ParseMoneyLine(period.Select("moneyLine"));
+
+                yield return new Period { Description = name, Spreads = pperiods, Totals = ptotals, MoneyLine = moneyLine };
+            }
 
 
+        }
+
+        private static MoneyLine ParseMoneyLine(XPathNodeIterator xMoneLine)
+        {
+            if (!xMoneLine.MoveNext())
+                return null;
+
+            var awayPrice = getFirstAndParseToDouble(GetChildNodeValue(xMoneLine.Current, "awayPrice"));
+            var homePrice = getFirstAndParseToDouble(GetChildNodeValue(xMoneLine.Current, "homePrice"));
+            var drawPrice = getFirstAndParseToDouble(GetChildNodeValue(xMoneLine.Current, "drawPrice"));
+
+            return new MoneyLine { AwayPrice = awayPrice, HomePrice = homePrice, DrawPrice = drawPrice };
+        }
+
+        private static IEnumerable<Total> ParseTotals(XPathNodeIterator totals)
+        {
+            while (totals.MoveNext())
+            {
+                var total = totals.Current;
+
+                var points = getFirstAndParseToDouble(GetChildNodeValue(total, "points"));
+                var overPrice = getFirstAndParseToDouble(GetChildNodeValue(total, "overPrice"));
+                var underPrice = getFirstAndParseToDouble(GetChildNodeValue(total, "underPrice"));
+
+                yield return new Total { Points = points, OverPrice = overPrice, UnderPrive = underPrice };
+            }
+        }
+
+        private static IEnumerable<Spread> ParseSpreads(XPathNodeIterator spreads)
+        {
+            while (spreads.MoveNext())
+            {
+                var spread = spreads.Current;
+                var awaySpread = getFirstAndParseToDouble(GetChildNodeValue(spread, "awaySpread"));
+                var awayPrice = getFirstAndParseToDouble(GetChildNodeValue(spread, "awayPrice"));
+                var homeSpread = getFirstAndParseToDouble(GetChildNodeValue(spread, "homeSpread"));
+                var homePrice = getFirstAndParseToDouble(GetChildNodeValue(spread, "homePrice"));
+
+                yield return new Spread { AwaySpread = awaySpread, AwayPrice = awayPrice, HomePrice = homePrice, HomeSpread = homeSpread };
+            }
         }
 
         static int GetIdFromNode(XPathNavigator nav)
@@ -105,18 +153,22 @@ namespace luckybird
 
     public class FootballEvent
     {
-        public string HomeTeam { get; set; }
-        public string AwayTeam { get; set; }
-        public DateTimeOffset When { get; set; }
-        public IEnumerable<Period> Periods { get; set; }
+        public string Sport;
+        public string League;
+        public string HomeTeam;
+        public string AwayTeam;
+        public DateTimeOffset When;
+        public IEnumerable<Period> Periods;
     }
 
     public class Period
     {
-        public DateTimeOffset Cutoff { get; set; }
-        public byte Number { get; set; }
-        public IEnumerable<Spread> Spreads { get; set; }
-        public IEnumerable<Total> Totals { get; set; }
+        public DateTimeOffset Cutoff;
+        public byte Number;
+        public string Description;
+        public IEnumerable<Spread> Spreads;
+        public IEnumerable<Total> Totals;
+        public MoneyLine MoneyLine;
 
     }
     public class Spread
@@ -149,27 +201,55 @@ namespace luckybird
     [TestFixture]
     public class Pinnacle_parser_should
     {
-        //{sport}{league}{home}{away}{when}{event}
+        //{sport}{league}
+        //{home}{away}{when}
+        //{event}
 
         [Test]
-        public void identify_leages_content()
+        public void parse_feed_content()
         {
-            var parser = new PinnacleFeedParser();
+            var parser = new PinnacleFeedParser(id => "sport" + id, id => "league" + id);
 
-            parser.Parse(@"D:\projects\luckybird\luckybird\luckybird\pinnacle.soccer.oneevent.xml");
-
-
+            var events = parser.Parse(@"D:\projects\luckybird\luckybird\luckybird\pinnacle.soccer.oneevent.xml").ToArray();
 
 
-            //var football = pinnacleFeed.Sports
-            //                    .Where(s => s.id == 29)
-            //                    .SelectMany(s => s.Leagues)
-            //                    .Where(l => l.id == 2242)
-            //                    .SelectMany(l => l.Events).First();
+            foreach (var @event in events)
+            {
+                Assert.AreEqual("sport29", @event.Sport);
+                Assert.AreEqual("league2242", @event.League);
+                Assert.AreEqual("AwayTeam", @event.AwayTeam);
+                Assert.AreEqual("HomeTeam", @event.HomeTeam);
+                Assert.AreEqual(DateTimeOffset.Parse("2013-01-12T01:29:00Z"), @event.When);
+                Assert.AreEqual(2, @event.Periods.Count());
 
+                foreach (var period in @event.Periods)
+                {
+                    foreach (var spread in period.Spreads)
+                    {
+                        Assert.AreNotEqual(0, spread.AwayPrice);
+                        Assert.AreNotEqual(0, spread.AwaySpread);
+                        Assert.AreNotEqual(0, spread.HomeSpread);
+                        Assert.AreNotEqual(0, spread.AwayPrice);
+                    }
 
+                    foreach (var total in period.Totals)
+                    {
+                        Assert.AreNotEqual(0, total.Points);
+                        Assert.AreNotEqual(0, total.OverPrice);
+                        Assert.AreNotEqual(0, total.UnderPrive);
+                    }
+
+                    if (period.MoneyLine != null)
+                    {
+                        Assert.AreEqual(2.74, period.MoneyLine.AwayPrice);
+                        Assert.AreEqual(2.74, period.MoneyLine.HomePrice);
+                        Assert.AreEqual(3.34, period.MoneyLine.DrawPrice);
+                    }
+                }
+            }
 
         }
     }
 }
+
 
